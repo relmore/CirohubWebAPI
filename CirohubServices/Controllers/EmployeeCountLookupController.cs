1bdc5752abd23bba1e6e9f61caafed591b223be0
Rodney <relmore@cirohub.com>
2017-01-07 23:00:44 -0500

    Added new controller

---
+++ CirohubServices/Controllers/EmployeeCountLookupController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CirohubServicesDataLayer;
+
+namespace CirohubServices.Controllers
+{
+    public class EmployeeCountLookupController : ApiController
+    {
+        public IEnumerable<EmployeeCountLookup> Get()
+        {
+
+            using (CirohubDBEntities entities = new CirohubDBEntities())
+            {
+
+                return entities.EmployeeCountLookups.ToList();
+            }
+        }
+
+        public EmployeeCountLookup Get(int id)
+        {
+            using (CirohubDBEntities entities = new CirohubDBEntities())
+            {
+
+                return entities.EmployeeCountLookups.FirstOrDefault(e => e.EmployeeCountID == id);
+            }
+        }
+    }
+}
